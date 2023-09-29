fdd66fedabf541f4b7aab9d43c3cb37f19e60c1d
Prasad <prasadhande@gmail.com>
2023-09-29 17:09:47 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+// function sayHello(name){
+//     console.log('Hello ' + name);
+// }
+
+// sayHello('Prasad...!!!');
+
+const logger = require('./logger'); 
+logger = 9;
+
+
+logger.log('Message');
